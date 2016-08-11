dd91e9e10f632847bcc9d05958f30d5180da2ec0
arquinn <arquinn@umich.edu>
2016-08-10 23:28:07 -0400

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -22,21 +22,21 @@ $(window).scroll(function(){
   var sticky = $('.navbar'),
       scroll = $(window).scrollTop();
 
-  var about = $('#About')
-  var contact = $("#Contact")
-  var beer = $("#Beer")
-  
-  var coff = contact.offset().top;
-  var boff = beer.offset().top
-  
-  var indicator = $("#indicator")
   if (scroll >= stickyOffset) sticky.addClass('fixed');
   else sticky.removeClass('fixed');
   
   
-  if ((boff - scroll) < 0) indicator[left] = 100
-  else if ((coff - scroll) < 0) indicator[left] = 200
-  else indicator[left] = 300
+
+  var about = $('#About');
+  var contact = $("#Contact");
+  var beer = $("#Beer");
+  var coff = contact.offset().top;
+  var boff = beer.offset().top;
+  var indicator = $("#indicator");
+  
+  if ((boff - scroll) < 0) indicator.attr("left", 100)
+  else if ((coff - scroll) < 0) indicator.attr("left",200)
+  else indicator.attr("left",300)
     
       
   
